a9a5b4c291ab09dba6f46fe608e4aeda0a67bbd3
Jon Pullinger <35332954+JonPullinger@users.noreply.github.com>
2018-09-03 22:05:04 +0200

    Create config.sample.js

---
+++ server/config.sample.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'make it wei(n)',
+    email: 'makeitwein@gmail.com',
+    description: '',
+    logo: '<svg></svg>',
+    google_analytics: 'todo',
+    erc20: {
+        contract: '0xf230b790e05390fc8295f4d3f60332c93bed42e2',
+        decimals: 6,
+        name: 'Tronix',
+        symbol: 'TRX',
+        // Find it on https://api.coinmarketcap.com/v2/listings/
+        coinmarketcap_id: 1958,
+    },
+    infura_apikey: 'kMmiacxGbli86XQnbPBF',
+    etherscan_apikey: 'T9XNQQ294E7ET62S7VSURQKF6WC332Q8AC',
+    auth: {
+        name: 'test',
+        pass: 'test',
+    },
+    network: 'homestead', // prod: homestead
+}
